80f2602b1067edf50778af3764770416d44ead7c
Gauri Wakchaure <gwakchaure1122@gmail.com>
2022-09-26 15:15:55 +0530

    js changes

--- app.js
+++ app.js
@@ -98,7 +98,8 @@ $(document).ready(function () {
         dots: false,
         responsive: {
             0: {
-                items: 1
+                items: 1,
+                nav:false
             },
 
             600: {
